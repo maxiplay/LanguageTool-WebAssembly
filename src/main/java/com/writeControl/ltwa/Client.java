7c884fdda5898ed13d17604c2b7b63aa86ab9923
Maxime <maxime.dhalluin@gmail.com>
2018-12-30 19:07:14 +0100

    Test without spellcheck

--- src/main/java/com/writeControl/ltwa/Client.java
+++ src/main/java/com/writeControl/ltwa/Client.java
@@ -6,9 +6,12 @@ import org.languagetool.language.French;
 import org.languagetool.rules.RuleMatch;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
 
 public class Client {
+
     public static void main(String[] args) throws IOException {
 
         Language language = new French();
@@ -19,9 +22,27 @@ public class Client {
        /* langTool.disableRule(HunspellRule.RULE_ID);
         langTool.disableRule(HunspellNoSuggestionRule.RULE_ID);*/
 
-        List<RuleMatch> matchs = langTool.check("Les enfant aiment la soupe");
+       try{
 
-        System.out.println(matchs);
+           List<RuleMatch> matchs = langTool.check("Les enfant aiment la soupe");
+           System.out.println(matchs);
+
+       }catch(Exception e){
+
+           System.out.println(e);
+
+           System.out.println(e.getMessage());
+           System.out.println(e.getCause());
+
+           StringWriter errors = new StringWriter();
+           e.printStackTrace(new PrintWriter(errors));
+
+           System.out.println( errors.toString());
+
+        }
+
+
+
 
     }
 }
